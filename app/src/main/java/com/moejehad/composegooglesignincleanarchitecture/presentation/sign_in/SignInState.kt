dfdbccc008538deb1043dadbdfd59e2bc894ffa8
moejehad <mohammedmadhoun18@gmail.com>
2023-04-10 02:21:20 +0300

    Google Sign In With Compose

---
+++ app/src/main/java/com/moejehad/composegooglesignincleanarchitecture/presentation/sign_in/SignInState.kt
@@ -0,0 +1,6 @@
+package com.moejehad.composegooglesignincleanarchitecture.presentation.sign_in
+
+data class SignInState(
+    val isSignInSuccessful: Boolean = false,
+    val signInErrorMessage: String? = null
+)
---
+++ app/src/main/java/com/moejehad/composegooglesignincleanarchitecture/presentation/sign_in/SignInViewModel.kt
@@ -0,0 +1,25 @@
+package com.moejehad.composegooglesignincleanarchitecture.presentation.sign_in
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SignInViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(SignInState())
+    val state = _state.asStateFlow()
+
+    fun onSignInResult(result: SignInResult) {
+        _state.update {
+            it.copy(
+                isSignInSuccessful = result.data != null,
+                signInErrorMessage = result.errorMessage
+            )
+        }
+    }
+
+    fun resetState() {
+        _state.update { SignInState() }
+    }
+}
